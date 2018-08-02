b909045c35b682b4078a4931cc3258aeb2096741
Thales Flores <thales@libring.com>
2018-08-01 23:35:48 -0300

    add functions that really create image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,5 +1,4 @@
 defmodule Identicon do
-  import IEx
   @moduledoc """
   Documentation for Identicon.
   """
@@ -19,6 +18,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   defp hash_input(input) do
@@ -53,5 +55,38 @@ defmodule Identicon do
       |> Enum.filter(fn ({code, _index}) -> rem(code, 2) == 0 end)
 
     %Identicon.Image{image | grid: odd_grid}
+  end
+
+  defp build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      grid
+      |> Enum.map(fn ({_code, index}) -> calculate_vertices(index) end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  defp calculate_vertices(index) do
+    horizontal = rem(index, 5) * 50
+    vertical = div(index, 5) * 50
+
+    top_left = {horizontal, vertical}
+    bottom_right = {horizontal + 50, vertical + 50}
+
+    {top_left, bottom_right}
+  end
+
+  defp draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  defp save_image(image, filename) do
+    File.write("#{filename}.png", image)
   end
 end
